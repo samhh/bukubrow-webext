d329e488c55bda3e49179477dc8bcf27c6ab8066
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2017-02-25 17:18:23 +0000

    Utilise local storage, and click to request new bookmarks

--- webextension/backend.js
+++ webextension/backend.js
@@ -5,15 +5,28 @@ const cfg = {
 	timerIntervalInSecs: 5
 }
 
+// Save bookmarks to local storage
+const saveBookmarks = bookmarks => {
+	return new Promise((resolve, reject) => {
+		chrome.storage.local.set({ bookmarks }, resolve)
+	})
+}
+
+// Tell frontend that updated bookmarks are available from local storage
+const sendBookmarksNotif = () => {
+	chrome.runtime.sendMessage({ bookmarksUpdated: true })
+}
+
 // Request bookmarks
 const requestBookmarks = () => {
 	console.log('Bookmarks requested...')
 
 	chrome.runtime.sendNativeMessage(cfg.appName, { request: 'true' }, res => {
-		if (chrome.runtime.lastError) console.log(chrome.runtime.lastError)
-		else console.log('...bookmarks fetched and returned.')
+		if (chrome.runtime.lastError) return console.log(chrome.runtime.lastError)
 
-		chrome.runtime.sendMessage({ bookmarks: res })
+		console.log('...bookmarks fetched and returned.')
+
+		saveBookmarks(res).then(sendBookmarksNotif)
 	})
 }
 
--- webextension/frontend.js
+++ webextension/frontend.js
@@ -3,17 +3,25 @@ console.log('Frontend loaded.')
 // Helper functions
 ensureValidURL = url => url.substring(0, 6) === 'http://' || url.substring(0, 7) === 'https://' ? url : `http://${url}`
 
+// Fetch bookmarks from local storage
+const fetchBookmarks = () => {
+	return new Promise((resolve, reject) => {
+		chrome.storage.local.get('bookmarks', resolve)
+	})
+}
+
 // Update bookmarks list
 const bookmarksEl = document.querySelector('.js-bookmarks')
 const updateBookmarks = bookmarks => {
 	bookmarksEl.innerHTML = ''
 
+	bookmarks = bookmarks.bookmarks
 	bookmarks.forEach(bookmark => {
 		const newEl = document.createElement('li')
 		newEl.innerHTML = `
-			<li>
+			<span>
 				${bookmark.Url}
-			</li>
+			</span>
 		`
 		newEl.addEventListener('click', () => {
 			chrome.tabs.create({ url: ensureValidURL(bookmark.Url) })
@@ -23,7 +31,7 @@ const updateBookmarks = bookmarks => {
 	})
 }
 
-// Request fresh bookmarks
+// Request updated bookmarks
 const requestBookmarks = () => {
 	chrome.runtime.sendMessage({ requestBookmarks: true })
 }
@@ -33,5 +41,8 @@ reqEl.addEventListener('click', requestBookmarks)
 
 // React to messages from backend
 chrome.runtime.onMessage.addListener(req => {
-	if (req.bookmarks) updateBookmarks(req.bookmarks)
+	if (req.bookmarksUpdated) fetchBookmarks().then(updateBookmarks)
 })
+
+// Load bookmarks saved in local storage on load
+fetchBookmarks().then(updateBookmarks)
