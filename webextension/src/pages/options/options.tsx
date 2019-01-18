fbd64910259c4edf014371a9b8315c20d542a3f5
Sam A. Horvath-Hunt <git@samhh.com>
2019-01-18 18:04:49 +0000

    Convert options page to functional component w/ hooks; add options page padding

--- webextension/src/pages/options/options.tsx
+++ webextension/src/pages/options/options.tsx
@@ -1,44 +1,38 @@
-import React, { Component, FormEvent } from 'react';
+import React, { FormEvent, SFC, useState, useEffect } from 'react';
 import { getSettings, saveSettings, Settings, Theme } from 'Modules/settings';
+import s from './options.css';
 
-type State = Settings;
+const OptionsPage: SFC<{}> = () => {
+	const [theme, setThemeState] = useState(Theme.Light);
 
-class OptionsPage extends Component<{}, State> {
-	state = {
-		theme: Theme.Light,
+	const setSettings = (settings: Partial<Settings>) => {
+		if (settings.theme) setThemeState(settings.theme);
+
+		saveSettings(settings);
 	};
 
-	async componentDidMount() {
-		const settings = await getSettings();
+	useEffect(() => {
+		getSettings().then(setSettings);
+	}, []);
 
-		// Type assertion because React state typings dislike Partials
-		this.setState(settings as Settings, this.syncState);
-	}
-
-	syncState() {
-		saveSettings(this.state);
-	}
-
-	handleThemeChange = (evt: FormEvent<HTMLSelectElement>) => {
-		// Type assertion as we wholly control this value
+	const handleThemeChange = (evt: FormEvent<HTMLSelectElement>) => {
+		// Type assertion is safe as we wholly control this value
 		const theme = evt.currentTarget.value as Theme;
 
-		this.setState({ theme }, this.syncState);
-	}
+		setSettings({ theme });
+	};
 
-	render() {
-		return (
-			<main>
-				<select
-					value={this.state.theme}
-					onChange={this.handleThemeChange}
-				>
-					<option value={Theme.Light}>Light</option>
-					<option value={Theme.Dark}>Dark</option>
-				</select>
-			</main>
-		);
-	}
-}
+	return (
+		<main className={s.page}>
+			<select
+				value={theme}
+				onChange={handleThemeChange}
+			>
+				<option value={Theme.Light}>Light</option>
+				<option value={Theme.Dark}>Dark</option>
+			</select>
+		</main>
+	);
+};
 
 export default OptionsPage;
