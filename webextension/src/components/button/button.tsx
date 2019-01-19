8ce8c47453e9f794a65ef485ab8e8f98dc6589a1
Sam A. Horvath-Hunt <git@samhh.com>
2019-01-19 23:27:23 +0000

    SFC ALL THE THINGS

--- webextension/src/components/button/button.tsx
+++ webextension/src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent, forwardRef, Ref, MouseEvent } from 'react';
+import React, { forwardRef, Ref, SFC, MouseEvent } from 'react';
 import cn from 'classnames';
 import styles from './button.css';
 
@@ -21,42 +21,31 @@ interface PropsWithIcon extends BaseProps {
 
 type Props = XOR<PropsWithLabel, PropsWithIcon>;
 
-class Button extends PureComponent<Props> {
-	static defaultProps: Partial<Props> = {
-		tooltip: '',
-		type: 'button',
-		className: '',
-		tooltipClassName: '',
-	};
+const Button: SFC<Props> = props => (
+	<button
+		className={cn(
+			styles.btn,
+			{ [styles['btn--icon']]: !!props.iconHTML },
+			props.className,
+		)}
+		type={props.type || 'button'}
+		onClick={props.onClick}
+		ref={props.forwardedRef}
+	>
+		{props.label}
 
-	render() {
-		return (
-			<button
-				className={cn(
-					styles.btn,
-					{ [styles['btn--icon']]: !!this.props.iconHTML },
-					this.props.className,
-				)}
-				type={this.props.type}
-				onClick={this.props.onClick}
-				ref={this.props.forwardedRef}
-			>
-				{this.props.label}
+		{props.iconHTML && (
+			<span
+				className={styles.icon}
+				dangerouslySetInnerHTML={{ __html: props.iconHTML }}
+			/>
+		)}
 
-				{this.props.iconHTML && (
-					<span
-						className={styles.icon}
-						dangerouslySetInnerHTML={{ __html: this.props.iconHTML }}
-					/>
-				)}
-
-				{this.props.tooltip && (
-					<span className={cn(styles.tooltip, this.props.tooltipClassName)}>{this.props.tooltip}</span>
-				)}
-			</button>
-		);
-	}
-}
+		{props.tooltip && (
+			<span className={cn(styles.tooltip, props.tooltipClassName)}>{props.tooltip}</span>
+		)}
+	</button>
+);
 
 export default forwardRef((props: Props, ref?: Ref<HTMLButtonElement>) => (
 	<Button forwardedRef={ref} {...props} />
