fc7615ff9785893b43dacbed1a55d39e41262496
Sam A. Horvath-Hunt <git@samhh.com>
2019-02-02 18:34:50 +0000

    rustfmt

--- binary/src/buku.rs
+++ binary/src/buku.rs
@@ -1,18 +1,16 @@
-use std::path::PathBuf;
-use std::io;
 use std::env;
+use std::io;
+use std::path::PathBuf;
 
 // Determine path to database from environment variables
 pub fn get_db_path() -> Result<PathBuf, io::Error> {
-	let db_filename = "bookmarks.db";
+    let db_filename = "bookmarks.db";
 
     let dir: Result<PathBuf, env::VarError> = match env::var("XDG_DATA_HOME") {
-        Ok(xdg_home) => { Ok(PathBuf::from(xdg_home + "/buku/")) },
-        _ => {
-            match env::var("HOME") {
-                Ok(home) => { Ok(PathBuf::from(home + "/.local/share/buku/")) },
-                Err(err) => Err(err),
-            }
+        Ok(xdg_home) => Ok(PathBuf::from(xdg_home + "/buku/")),
+        _ => match env::var("HOME") {
+            Ok(home) => Ok(PathBuf::from(home + "/.local/share/buku/")),
+            Err(err) => Err(err),
         },
     };
 
@@ -21,6 +19,9 @@ pub fn get_db_path() -> Result<PathBuf, io::Error> {
 
         return Ok(path);
     } else {
-        return Err(io::Error::new(io::ErrorKind::NotFound, "Failed to find Buku database."));
+        return Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "Failed to find Buku database.",
+        ));
     }
 }
