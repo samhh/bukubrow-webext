16d6c1810d1f5733b32385a124e4eecb298901dc
Sam A. Horvath-Hunt <github@samhh.com>
2019-02-02 19:53:42 +0000

    Add Azure CI cfg

--- binary/src/database.rs
+++ binary/src/database.rs
@@ -1,5 +1,5 @@
-use std::path::PathBuf;
 use rusqlite::{Connection, Error as DbError};
+use std::path::PathBuf;
 
 pub type BookmarkId = i32;
 
@@ -32,15 +32,13 @@ impl SqliteDatabase {
         let query = "SELECT * FROM bookmarks;";
         let mut stmt = self.connection.prepare(query)?;
 
-        let rows = stmt.query_map(&[], |row| {
-            Bookmark {
-                id: row.get(0),
-                url: row.get(1),
-                metadata: row.get(2),
-                tags: row.get(3),
-                desc: row.get(4),
-                flags: row.get(5),
-            }
+        let rows = stmt.query_map(&[], |row| Bookmark {
+            id: row.get(0),
+            url: row.get(1),
+            metadata: row.get(2),
+            tags: row.get(3),
+            desc: row.get(4),
+            flags: row.get(5),
         })?;
 
         let bookmarks: Vec<Bookmark> = rows.filter_map(|x| x.ok()).collect();
@@ -50,8 +48,12 @@ impl SqliteDatabase {
 
     // Save bookmark to database
     pub fn add_bookmark(&self, bm: &Bookmark) -> Result<usize, DbError> {
-        let query = "INSERT INTO bookmarks(metadata, desc, tags, url, flags) VALUES (?1, ?2, ?3, ?4, ?5);";
-        let exec = self.connection.execute(query, &[&bm.metadata, &bm.desc, &bm.tags, &bm.url, &bm.flags]);
+        let query =
+            "INSERT INTO bookmarks(metadata, desc, tags, url, flags) VALUES (?1, ?2, ?3, ?4, ?5);";
+        let exec = self.connection.execute(
+            query,
+            &[&bm.metadata, &bm.desc, &bm.tags, &bm.url, &bm.flags],
+        );
 
         exec
     }
@@ -59,7 +61,17 @@ impl SqliteDatabase {
     // Update bookmark in database by ID
     pub fn update_bookmark(&self, bm: &Bookmark) -> Result<usize, DbError> {
         let query = "UPDATE bookmarks SET (metadata, desc, tags, url, flags) = (?2, ?3, ?4, ?5, ?6) WHERE id = ?1;";
-        let exec = self.connection.execute(query, &[&bm.id.unwrap(), &bm.metadata, &bm.desc, &bm.tags, &bm.url, &bm.flags]);
+        let exec = self.connection.execute(
+            query,
+            &[
+                &bm.id.unwrap(),
+                &bm.metadata,
+                &bm.desc,
+                &bm.tags,
+                &bm.url,
+                &bm.flags,
+            ],
+        );
 
         exec
     }
