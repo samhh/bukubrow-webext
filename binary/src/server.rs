8bc365bf79792a8e72a5d19562b8769a2b8b553c
Sam A. Horvath-Hunt <git@samhh.com>
2019-02-03 12:41:49 +0000

    Rust 2018 edition idioms

--- binary/src/server.rs
+++ binary/src/server.rs
@@ -1,6 +1,6 @@
 use chrome_native_messaging::{errors, event_loop, write_output};
-use config::VERSION;
-use database::{Bookmark, BookmarkId, SqliteDatabase};
+use crate::config::VERSION;
+use crate::database::{Bookmark, BookmarkId, SqliteDatabase};
 use serde_json;
 use std::error::Error;
 use std::io;
