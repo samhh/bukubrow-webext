86b9a338d2881376b7d3c7fd12e0f35d501dbbf9
Sam A. Horvath-Hunt <git@samhh.com>
2019-12-03 18:43:15 +0000

    Add asArray helper

--- src/modules/array.ts
+++ src/modules/array.ts
@@ -8,3 +8,5 @@ export const lookupC = (i: number) => <T>(xs: T[]): O.Option<T> => A.lookup(i, x
  */
 export const snoc_ = <T>(xs: T[]) => (y: T): T[] => xs.concat(y);
 
+export const asArray = <A>(xs: A | Array<A>): Array<A> => Array.isArray(xs) ? xs : [xs];
+
