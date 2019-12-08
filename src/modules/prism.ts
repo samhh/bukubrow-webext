50fb1c378864e5455551f4de0824d9912b98d0f9
Sam A. Horvath-Hunt <git@samhh.com>
2019-12-08 20:41:34 +0000

    Refactor endIndexOfAnyOf

---
+++ src/modules/prism.ts
@@ -0,0 +1,10 @@
+import { Endomorphism } from 'fp-ts/lib/function';
+import { Prism } from 'monocle-ts';
+
+export const prismModifyOption = <A, B>(p: Prism<A, B>) => (f: Endomorphism<A>) => (x: B): Option<B> => pipe(
+	x,
+	p.reverseGet,
+	f,
+	p.getOption,
+);
+
--- src/modules/string.ts
+++ src/modules/string.ts
@@ -1,6 +1,10 @@
+import { pipe } from 'fp-ts/lib/pipeable';
 import { Predicate } from 'fp-ts/lib/function';
 import * as O from 'fp-ts/lib/Option';
+import * as A from 'fp-ts/lib/Array';
 import { prismNonNegativeInteger, NonNegativeInteger } from 'newtype-ts/lib/NonNegativeInteger';
+import { prismModifyOption } from 'Modules/prism';
+import { add } from 'Modules/math';
 
 export const includes = (x: string): Predicate<string> => (y): boolean => y.includes(x);
 
@@ -14,20 +18,20 @@ export const includesCI = (x: string): Predicate<string> => (y): boolean =>
  * Return ending index (index plus one) of the first match from a series of
  * tests.
  */
-// TODO
-export const endIndexOfAnyOf = (x: string) => (ys: Array<string>): Option<NonNegativeInteger> => {
-	for (const searchString of ys) {
-		const index = x.indexOf(searchString);
-
-		if (index !== -1) return prismNonNegativeInteger.getOption(index + searchString.length);
-	}
-
-	return O.none;
-};
+export const endIndexOfAnyOf = (x: string) => (ys: Array<string>): Option<NonNegativeInteger> => pipe(
+	ys,
+	A.findFirstMap((y) => pipe(
+		indexOf(y)(x),
+		O.chain(prismModifyOption(prismNonNegativeInteger)(add(y.length))),
+	)),
+);
 
 export const fromNumber = (x: number): string => String(x);
 
 export const take = (i: number) => (x: string): string => x.substring(i);
 
 export const split = (x: string | RegExp) => (y: string): Array<string> => y.split(x);
+
+export const indexOf = (x: string) => (y: string): Option<NonNegativeInteger> =>
+	pipe(y.indexOf(x), prismNonNegativeInteger.getOption);
 
