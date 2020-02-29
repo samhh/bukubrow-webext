46a3d210e9436854721858f613ba53e88f9aad4d
Sam A. Horvath-Hunt <git@samhh.com>
2020-02-29 18:21:29 +0000

    Improve regex tests

--- test/Data/String/Regex.purs
+++ test/Data/String/Regex.purs
@@ -12,16 +12,22 @@ import Test.Spec.Assertions (shouldEqual)
 spec :: Spec Unit
 spec = describe "Data.String.Regex" do
     describe "matchAllFirstGroups" do
-        let r = unsafeRegex "(?:X)(a.c)"
+        let r = unsafeRegex "(a.c)"
         let rn = r noFlags
         let rg = r global
 
         it "matches none" do
-            matchAllFirstGroups rg "abc" `shouldEqual` fromArray []
+            matchAllFirstGroups rg "ab" `shouldEqual` fromArray []
         it "matches one" do
-            matchAllFirstGroups rg "blah Xabc blah" `shouldEqual` fromArray ["abc"]
+            matchAllFirstGroups rg "blah abc blah" `shouldEqual` fromArray ["abc"]
         it "matches many" do
-            matchAllFirstGroups rg "blah Xabc blah Xayc blah" `shouldEqual` fromArray ["abc", "ayc"]
+            matchAllFirstGroups rg "blah abc blah ayc blah" `shouldEqual` fromArray ["abc", "ayc"]
         it "matches only first without global flag" do
-            matchAllFirstGroups rn "blah Xabc blah Xayc blah" `shouldEqual` fromArray ["abc"]
+            matchAllFirstGroups rn "blah abc blah ayc blah" `shouldEqual` fromArray ["abc"]
+        it "does not match non-capture groups" do
+            let r2 = unsafeRegex "(?:X)(a.c)" global
+            matchAllFirstGroups r2 "blah Xabc blah" `shouldEqual` fromArray ["abc"]
+        it "does not match other capture groups" do
+            let r2 = unsafeRegex "(a.c)(X)" global
+            matchAllFirstGroups r2 "blah abcX blah" `shouldEqual` fromArray ["abc"]
 
