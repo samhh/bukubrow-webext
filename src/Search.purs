c65ddafd1ed20d8c2407687c6aaeee59825565a2
Sam A. Horvath-Hunt <git@samhh.com>
2020-02-29 12:03:02 +0000

    Add Search module

---
+++ src/Search.purs
@@ -0,0 +1,47 @@
+module Search where
+
+import Prelude
+
+import Control.Monad.Custom (filter)
+import Data.Array.NonEmpty (NonEmptyArray, head)
+import Data.Maybe (Maybe(..))
+import Data.String.Regex (Regex, match)
+import Data.String.Regex.Custom (matchAllFirstGroups)
+import Data.String.Regex.Flags (global, noFlags)
+import Data.String.Regex.Unsafe (unsafeRegex)
+
+nameR :: Regex
+nameR = unsafeRegex     "^.*?(?:(?=^[#>:*].+)|(?= +[#>:*].+)|$)" noFlags
+
+descR :: Regex
+descR = unsafeRegex     "(?:^| )>(.+?)(?= +[#>:*]|$)"            global
+
+urlR :: Regex
+urlR = unsafeRegex      "(?:^| ):(.+?)(?= +[#>:*]|$)"            global
+
+tagsR :: Regex
+tagsR = unsafeRegex     "(?:^| )#(.+?)(?= +[#>:*]|$)"            global
+
+wildcardR :: Regex
+wildcardR = unsafeRegex "(?:^| )\\*(.+?)(?= +[#>:*]|$)"          global
+
+type Result =
+    { name :: Maybe String
+    , desc :: Maybe (NonEmptyArray String)
+    , url :: Maybe (NonEmptyArray String)
+    , tags :: Maybe (NonEmptyArray String)
+    , wildcard :: Maybe (NonEmptyArray String)
+    }
+
+empty :: Result
+empty = { name: Nothing, desc: Nothing, url: Nothing, tags: Nothing, wildcard: Nothing }
+
+parse :: String -> Result
+parse x =
+    { name: match nameR x >>= head >>> filter (_ /= "")
+    , desc: matchAllFirstGroups descR x
+    , url: matchAllFirstGroups urlR x
+    , tags: matchAllFirstGroups tagsR x
+    , wildcard: matchAllFirstGroups wildcardR x
+    }
+
---
+++ test/Search.purs
@@ -0,0 +1,63 @@
+module Search.Test where
+
+import Prelude
+
+import Data.Array.NonEmpty (fromArray)
+import Data.Maybe (Maybe(..))
+import Search (empty, parse)
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+
+spec :: Spec Unit
+spec = describe "Search" do
+    describe "parse" do
+        it "parses nothing for empty string" do
+            parse "" `shouldEqual` empty
+        it "parses name alone" do
+            parse "abc" `shouldEqual` empty { name = Just "abc" }
+            parse "abc xyz" `shouldEqual` empty { name = Just "abc xyz" }
+        it "parses desc alone" do
+            parse ">abc"          `shouldEqual` empty { desc = fromArray ["abc"] }
+            parse ">abc xyz"      `shouldEqual` empty { desc = fromArray ["abc xyz"] }
+            parse ">abc >xyz"     `shouldEqual` empty { desc = fromArray ["abc", "xyz"] }
+            parse ">abc >xyz xyz" `shouldEqual` empty { desc = fromArray ["abc", "xyz xyz"] }
+        it "parses url alone" do
+            parse ":abc"          `shouldEqual` empty { url = fromArray ["abc"] }
+            parse ":abc xyz"      `shouldEqual` empty { url = fromArray ["abc xyz"] }
+            parse ":abc :xyz"     `shouldEqual` empty { url = fromArray ["abc", "xyz"] }
+            parse ":abc :xyz xyz" `shouldEqual` empty { url = fromArray ["abc", "xyz xyz"] }
+        it "parses tags alone" do
+            parse "#abc"          `shouldEqual` empty { tags = fromArray ["abc"] }
+            parse "#abc xyz"      `shouldEqual` empty { tags = fromArray ["abc xyz"] }
+            parse "#abc #xyz"     `shouldEqual` empty { tags = fromArray ["abc", "xyz"] }
+            parse "#abc #xyz xyz" `shouldEqual` empty { tags = fromArray ["abc", "xyz xyz"] }
+        it "parses wildcard alone" do
+            parse "*abc"          `shouldEqual` empty { wildcard = fromArray ["abc"] }
+            parse "*abc xyz"      `shouldEqual` empty { wildcard = fromArray ["abc xyz"] }
+            parse "*abc *xyz"     `shouldEqual` empty { wildcard = fromArray ["abc", "xyz"] }
+            parse "*abc *xyz xyz" `shouldEqual` empty { wildcard = fromArray ["abc", "xyz xyz"] }
+        it "parses multiple combined" do
+            let expected = empty {
+                  name = Just "name"
+                , desc = fromArray ["desc1", "desc no2"]
+                , url = fromArray ["url1", "url2"]
+                , tags = fromArray ["tag1", "tag2", "tag3", "tag4"]
+                , wildcard = fromArray ["wc1"]
+                }
+            parse "name #tag1 #tag2 #tag3 >desc1 :url1 >desc no2 *wc1 :url2 #tag4" `shouldEqual` expected
+        it "parses edge cases predictably" do
+            parse "a"      `shouldEqual` empty { name = Just "a" }
+            parse "a :"    `shouldEqual` empty { name = Just "a :" }
+            parse "a :u"   `shouldEqual` empty { name = Just "a", url = fromArray ["u"] }
+            parse "a \\:u" `shouldEqual` empty { name = Just "a \\:u" }
+            parse "a:a"    `shouldEqual` empty { name = Just "a:a" }
+            parse "a:a :"  `shouldEqual` empty { name = Just "a:a :" }
+            parse "a:a :u" `shouldEqual` empty { name = Just "a:a", url = fromArray ["u"] }
+            parse "aa"     `shouldEqual` empty { name = Just "aa" }
+            parse " a :"   `shouldEqual` empty { name = Just " a :" }
+            parse " a :u"  `shouldEqual` empty { name = Just " a", url = fromArray ["u"] }
+            parse " :"     `shouldEqual` empty { name = Just " :" }
+            parse " :u"    `shouldEqual` empty { url = fromArray ["u"] }
+            parse " :u :"  `shouldEqual` empty { url = fromArray ["u"] }
+            parse " :u :u" `shouldEqual` empty { url = fromArray ["u", "u"] }
+
