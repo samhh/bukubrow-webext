c814b8a0d12b13f73b8916200c1384c06f1369a4
Sam A. Horvath-Hunt <git@samhh.com>
2020-03-02 18:25:57 +0000

    Add Tag module

---
+++ src/Tag.purs
@@ -0,0 +1,61 @@
+-- | Tags are non-empty strings without commas (Buku's internal delimiter).
+
+module Tag where
+
+import Prelude
+
+import Buku (bukuTagDelimiterP)
+import Control.Alternative.Custom (ensure)
+import Data.Argonaut.Decode (class DecodeJson, decodeJson)
+import Data.Argonaut.Encode (class EncodeJson, encodeJson)
+import Data.Either (Either, note)
+import Data.Lens (Prism', preview, prism', review)
+import Data.Maybe (Maybe)
+import Data.Newtype (class Newtype, un)
+import Data.String.NonEmpty (NonEmptyString, contains)
+import Data.String.NonEmpty as NES
+import Test.QuickCheck (class Arbitrary, arbitrary)
+import Test.QuickCheck.Gen.Custom (suchThatMap)
+
+newtype Tag = Tag NonEmptyString
+
+isTag :: NonEmptyString -> Boolean
+isTag = not $ contains bukuTagDelimiterP
+
+tagPrism :: Prism' NonEmptyString Tag
+tagPrism = prism' (un Tag) (ensure isTag >>> map Tag)
+
+derive instance newtypeTag :: Newtype Tag _
+
+instance showTag :: Show Tag where
+    show = toString
+
+instance eqTag :: Eq Tag where
+    eq x y = toNonEmptyString x == toNonEmptyString y
+
+instance arbitraryTag :: Arbitrary Tag where
+    arbitrary = arbitrary `suchThatMap` fromNonEmptyString
+
+instance encodeTag :: EncodeJson Tag where
+    encodeJson = toString >>> encodeJson
+
+instance decodeTag :: DecodeJson Tag where
+    decodeJson json = do
+        str <- decodeJson json :: Either String String
+        note "Value is not a Tag" (fromString str)
+
+fromNonEmptyString :: NonEmptyString -> Maybe Tag
+fromNonEmptyString = preview tagPrism
+
+fromString :: String -> Maybe Tag
+fromString = NES.fromString >=> fromNonEmptyString
+
+toNonEmptyString :: Tag -> NonEmptyString
+toNonEmptyString = review tagPrism
+
+toString :: Tag -> String
+toString = toNonEmptyString >>> NES.toString
+
+length :: Tag -> Int
+length = toNonEmptyString >>> NES.length
+
---
+++ test/Tag.purs
@@ -0,0 +1,21 @@
+module Tag.Test where
+
+import Prelude
+
+import Buku (bukuTagDelimiterP)
+import Data.Lens (preview, review)
+import Data.Maybe (isJust)
+import Data.String.NonEmpty (NonEmptyString, contains)
+import Tag (tagPrism)
+import Test.QuickCheck ((===))
+import Test.Spec (Spec, describe, it)
+import Test.Spec.QuickCheck (quickCheck)
+
+spec :: Spec Unit
+spec = describe "Tag" do
+    describe "tagPrism" do
+        let f = preview tagPrism
+        let g = review tagPrism
+        it "accepts non-empty strings only without delimiter" do
+            quickCheck \(x :: NonEmptyString) -> isJust (f x) === not (contains bukuTagDelimiterP x)
+
