cc319696f508c5ca831f9fe6ea838a0b5706d211
Sam A. Horvath-Hunt <git@samhh.com>
2020-03-11 23:33:04 +0000

    Add Badge component

---
+++ src/Ui/Badge.purs
@@ -0,0 +1,26 @@
+module Ui.Badge (badge, Rank(..)) where
+
+import Prelude
+
+import Css (modifierM)
+import Data.Maybe (Maybe(..))
+import Halogen (ClassName(..))
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+
+data Rank
+    = Primary
+    | Secondary
+    | None
+
+cm :: Rank -> Maybe String
+cm Primary = Just "primary"
+cm Secondary = Just "secondary"
+cm None = Nothing
+
+cs :: Rank -> Array ClassName
+cs = modifierM (ClassName "badge") cm
+
+badge :: forall a b. Rank -> HH.HTML a b
+badge x = HH.span [ HP.classes $ cs x ] []
+
