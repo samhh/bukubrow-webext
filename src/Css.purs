9bd6d654be531a3e4583412aaf9765c00d7d3292
Sam A. Horvath-Hunt <git@samhh.com>
2020-03-11 18:03:40 +0000

    Add another BEM modifier fn; make them return an array including the non-modified

--- src/Css.purs
+++ src/Css.purs
@@ -2,6 +2,8 @@ module Css where
 
 import Prelude
 
+import Data.Array (catMaybes)
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Halogen (ClassName(..))
 import Types (Predicate)
@@ -11,8 +13,11 @@ foreign import addHtmlClass :: String -> Effect Unit
 foreign import removeHtmlClass :: String -> Effect Unit
 
 -- | Conditionally append a BEM modifier.
-modifier :: forall a. ClassName -> String -> Predicate a -> a -> ClassName
-modifier (ClassName c) m f x = if f x
-    then ClassName (c <> "--" <> m)
-    else ClassName c
+modifierM :: forall a. ClassName -> (a -> Maybe String) -> a -> Array ClassName
+modifierM (ClassName c) f x = [c] <> catMaybes [f x <#> ((c <> "--") <> _)] <#> ClassName
+
+-- | Conditionally append a static string BEM modifier dependent upon a
+-- | predicate.
+modifierP :: forall a. ClassName -> String -> Predicate a -> a -> Array ClassName
+modifierP c m f x = modifierM c (const (if f x then Just m else Nothing)) x
 
--- test/Css.purs
+++ test/Css.purs
@@ -2,7 +2,8 @@ module Css.Test where
 
 import Prelude
 
-import Css (modifier)
+import Css (modifierM, modifierP)
+import Data.Maybe (Maybe(..))
 import Halogen (ClassName(..))
 import Test.QuickCheck ((===))
 import Test.Spec (Spec, describe, it)
@@ -10,12 +11,18 @@ import Test.Spec.QuickCheck (quickCheck)
 
 spec :: Spec Unit
 spec = describe "CSS" do
-    describe "modifier" do
-        let f = ClassName
-        let g (ClassName x) = x
+    let f = ClassName
+    let g (ClassName x) = x
 
+    describe "modifierM" do
+        it "appends returned Just modifier" do
+            quickCheck \(x :: String) (y :: String) -> (modifierM (f x) (const $ Just y) unit <#> g) === [x, x <> "--" <> y]
+        it "does not append anything if Nothing returned" do
+            quickCheck \(x :: String) (y :: String) -> (modifierM (f x) (const Nothing) unit <#> g) === [x]
+
+    describe "modifierP" do
         it "appends modifier if predicate holds" do
-            quickCheck \(x :: String) (y :: String) -> g (modifier (f x) y (const true) unit) === x <> "--" <> y
+            quickCheck \(x :: String) (y :: String) -> (modifierP (f x) y (const true) unit <#> g) === [x, x <> "--" <> y]
         it "does not append modifier if predicate does not hold" do
-            quickCheck \(x :: String) (y :: String) -> g (modifier (f x) y (const false) unit) === x
+            quickCheck \(x :: String) (y :: String) -> (modifierP (f x) y (const false) unit <#> g) === [x]
 
