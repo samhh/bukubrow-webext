565a9a61aa26d19259cf4d3b07c2e0cfa46431aa
Sam A. Horvath-Hunt <git@samhh.com>
2020-03-22 16:16:14 +0000

    Add hushed decodeJson' helper

--- src/Data/Argonaut/Decode.purs
+++ src/Data/Argonaut/Decode.purs
@@ -1,7 +1,16 @@
 module Data.Argonaut.Decode.Custom where
 
+import Prelude
+
 import Data.Argonaut.Core (Json)
-import Data.Either (Either)
+import Data.Argonaut.Decode (class DecodeJson, decodeJson)
+import Data.Either (Either, hush)
+import Data.Maybe (Maybe)
 
 type Decoder a = Json -> Either String a
+
+type Decoder' a = Json -> Maybe a
+
+decodeJson' :: forall a. DecodeJson a => Decoder' a
+decodeJson' = decodeJson >>> hush
 
