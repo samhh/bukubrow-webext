80a513b2ba29081ccaef3e1ba6bebd743f5e48e5
Christian Dannie Storgaard <cybolic@gmail.com>
2021-03-25 22:52:29 +0200

    Fixed broked getting of header height

--- src/modules/scroll-window.ts
+++ src/modules/scroll-window.ts
@@ -1,9 +1,11 @@
+import { headerHeight as headerHeightInPx } from '~/containers/search-controls';
+const headerHeight = parseInt(headerHeightInPx);
+
 export const scrollToTop: IO<void> = () => {
 	window.scrollTo(0, 0);
 };
 
 export const scrollToEl = (el: HTMLElement): IO<void> => (): void => {
-	const headerHeight = parseInt(window.getComputedStyle(document.body).getPropertyValue('--header-height'), 10);
 
 	window.scrollTo(0, el.getBoundingClientRect().top - document.documentElement.getBoundingClientRect().top - headerHeight);
 };
