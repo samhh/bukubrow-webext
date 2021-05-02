0f4ed821cab0d5600634c489494f277e62bb6643
Sam A. Horvath-Hunt <hello@samhh.com>
2021-05-02 19:17:22 +0100

    Hook into omnibox API in backend

--- src/backend.ts
+++ src/backend.ts
@@ -3,6 +3,7 @@ import { flow, constVoid, constant } from "fp-ts/lib/function"
 import * as T from "fp-ts/lib/Task"
 import * as TO from "fp-ts-contrib/lib/TaskOption"
 import * as O from "fp-ts/lib/Option"
+import * as A from "fp-ts/Array"
 import { initBadgeAndListen } from "~/modules/badge"
 import {
   initContextMenusAndListen,
@@ -13,11 +14,53 @@ import {
   IsomorphicMessage,
   sendIsomorphicMessage,
 } from "~/modules/comms/isomorphic"
-import { openPopup } from "~/modules/comms/browser"
+import {
+  executeCodeInActiveTab,
+  getBookmarksFromLocalStorageInfallible,
+  openPopup,
+} from "~/modules/comms/browser"
 import { listenForCommands, Command } from "~/modules/command"
-import { runIO, runTask } from "~/modules/fp"
+import { runIO, runTask, runTask_ } from "~/modules/fp"
 import sleep from "~/modules/sleep"
 import { contextClickTabs, ContextMenuEntry } from "~/modules/context"
+import { browser, Omnibox } from "webextension-polyfill-ts"
+import { onOmniboxInput, onOmniboxSubmit } from "~/modules/omnibox"
+import { includesCI } from "~modules/string"
+import { mkBookmarkletCode } from "~modules/bookmarklet"
+
+export const omniboxSubmitHandler = (url: string) => (
+  d: Omnibox.OnInputEnteredDisposition,
+): IO<void> => () =>
+  pipe(
+    url,
+    mkBookmarkletCode,
+    O.fold(() => {
+      switch (d) {
+        case "currentTab":
+          return void browser.tabs.update({ url })
+        case "newForegroundTab":
+        case "newBackgroundTab":
+          return void browser.tabs.create({ url })
+      }
+    }, flow(executeCodeInActiveTab, runTask_)),
+  )
+
+runIO(
+  onOmniboxInput(input =>
+    pipe(
+      getBookmarksFromLocalStorageInfallible,
+      T.map(
+        A.filterMap(bm =>
+          includesCI(input)(bm.title)
+            ? O.some({ content: bm.url, description: bm.title })
+            : O.none,
+        ),
+      ),
+    ),
+  ),
+)
+
+runIO(onOmniboxSubmit(omniboxSubmitHandler))
 
 initBadgeAndListen().then(f => {
   runIO(
